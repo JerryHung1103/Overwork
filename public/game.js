fd22cce1ac83f45ccd670163e9ad570aca795ea5
JerryHung1103 <144596214+JerryHung1103@users.noreply.github.com>
2024-04-30 14:41:12 +0800

    Add the functionality where the player moves to a clicked location on the canvas

--- public/game.js
+++ public/game.js
@@ -6,56 +6,37 @@ let cvw=1600;
 let cvh=700;
 canvas.width=1600;
 canvas.height=700;
-
-
 const playerWidth=240/10;
 const playerHeight = 200/8;
 let playerImg=new Image();
-// let boxImg=new Image();
-let first=true;
-let socketID;
-
-playerImg.onload=()=>{
-        requestAnimationFrame(drawAnimation);
-}
-
-
+playerImg.onload=()=>{requestAnimationFrame(drawAnimation);}
 playerImg.src= '/image/player_sprite.png'
-
 let PlayerArray=[];
-
-
-function updatePlayer(players){
-    let array=[];
-    players.forEach(d=>{
-        array.push( {id: d.id, player:Player(ctx,playerImg,playerWidth,playerHeight,d.x,d.y,3,20, 'stayFront')})
-    })
-    return array;
-}
-socket.on('addPlayer',(id)=>{
-    
-    if(first==true){
-        socketID=id;
-        socket.emit('constructPlayer',{id:socketID, x: Math.floor(Math.random() * 500), y:Math.floor(Math.random() * 500)})
-        first=false;
+const players = {}
+let browserID;
+socket.on('getID',id=>browserID=id);
+socket.on('updatePlayers',(backend_players)=>{
+    for(const id in backend_players){
+        const pos = backend_players[id];
+        if(!players[id]){
+            players[id]=Player(ctx,playerImg,playerWidth,playerHeight,pos.x,pos.y,3,50, 'stayFront')
+        }
     }
-    console.log(socketID);
+    for(const id in players){
+        if(!backend_players[id]){
+            delete players[id];
+        }
+    }
 })
-
-
-socket.on('updatePlayer',players=>{
-    PlayerArray=updatePlayer(players);
-})
-
-
- 
 function drawAnimation(now){
     ctx.clearRect(0,0,cvw,cvh)
     PlayerArray.forEach(d=>d.player.update(now))
-    PlayerArray.forEach(d=>d.player.draw())
+    for(const id in players){
+        players[id].update(now);
+        players[id].draw();
+    }
     requestAnimationFrame(drawAnimation);
 }
-
 document.addEventListener('keydown',event=>{
     let keycode=event.keyCode;
     switch(keycode){
@@ -73,19 +54,18 @@ document.addEventListener('keydown',event=>{
             break;
     }
 });
-
-
 socket.on('moveByID_right',id=>{
-    PlayerArray.filter(d=>d.id==id).forEach(d=>d.player.moveRight());
+   
+    players[id].moveRight();
 })
 socket.on('moveByID_left',id=>{
-    PlayerArray.filter(d=>d.id==id).forEach(d=>d.player.moveLeft());
+    players[id].moveLeft();
 })
 socket.on('moveByID_up',id=>{
-    PlayerArray.filter(d=>d.id==id).forEach(d=>d.player.moveUp());
+    players[id].moveUp();
 })
 socket.on('moveByID_front',id=>{
-    PlayerArray.filter(d=>d.id==id).forEach(d=>d.player.moveFront());
+    players[id].moveFront();
 })
 
 document.addEventListener('keyup',event=>{
@@ -96,6 +76,200 @@ document.addEventListener('keyup',event=>{
 });
 
 socket.on('stopByID',id=>{
-    PlayerArray.filter(d=>d.id==id).forEach(d=>d.player.stop());
+    players[id].stop();
 })
+
+let state = {
+    moving:false,
+    id:0
+};
+
+canvas.addEventListener('click', function(event) {
+    if(state.moving)
+        clearInterval(state.id);
+        var rect = canvas.getBoundingClientRect();
+        var x = event.clientX - rect.left -  playerWidth;
+        var y = event.clientY - rect.top - playerHeight;
+        console.log('Clicked at: x=' + x + ', y=' + y);
+
+        let playerX= players[browserID].getX();
+        let playerY= players[browserID].getY();
+        let diffX = Math.abs(playerX - x);
+        let diffY = Math.abs(playerY - y);
+    
+
+        //1st quadrant
+        if(playerX<=x && playerY>=y){
+            state.moving=true;
+            if(diffX<=diffY){
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'right');
+                    console.log(players[browserID].getX())
+                    if(players[browserID].getX()>x - playerWidth){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'up');
+                            if(players[browserID].getY()<y){
+                                clearInterval(id);
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+            else{
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'up');
+                    if(players[browserID].getY()<y){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'right');
+                            console.log(players[browserID].getX())
+                            if(players[browserID].getX()>x- playerWidth){
+                                clearInterval(id);
+                               
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+        }
+         //2nd quadrant
+        else if(playerX>=x && playerY>=y){
+            state.moving=true;
+            if(diffX<=diffY){
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'left');
+                    console.log(players[browserID].getX())
+                    if(players[browserID].getX()<x){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'up');
+                            console.log(players[browserID].getY())
+                            if(players[browserID].getY()<y){
+                                clearInterval(id);
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+            else{ 
+                let id= setInterval(() => {
+                state.id=id;
+                socket.emit('move', 'up');
+                if(players[browserID].getY()<y){
+                    clearInterval(id);
+                    id= setInterval(() => {
+                        state.id=id;
+                        socket.emit('move', 'left');
+                        if(players[browserID].getX()<x){
+                            clearInterval(id);
+                            socket.emit('stop','');    
+                            state.moving=false; 
+                        }
+                    }, 50);  
+                }
+            }, 50);
+            }           
+        }
+
+        //3rd quadrant
+        else if(playerX>=x && playerY<=y){
+            state.moving=true;
+            if(diffX<=diffY){
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'left');
+                    console.log(players[browserID].getX())
+                    if(players[browserID].getX()<x){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'front');
+                            console.log(players[browserID].getY())
+                            if(players[browserID].getY()>y- playerHeight){
+                                clearInterval(id);
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+            else{
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'front');
+                    if(players[browserID].getY()>y- playerHeight){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'left');
+                            if(players[browserID].getX()<x){
+                                clearInterval(id);
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+        }
+        //4th quadrant
+        else if(playerX<=x && playerY<=y){
+            state.moving=true;
+            if(diffX<=diffY){
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'right');
+                    console.log(players[browserID].getX())
+                    if(players[browserID].getX()>x- playerWidth){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'front');
+                            console.log(players[browserID].getY())
+                            if(players[browserID].getY()>y- playerHeight){
+                                clearInterval(id);
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+            else{
+
+                let id= setInterval(() => {
+                    state.id=id;
+                    socket.emit('move', 'front');
+                    if(players[browserID].getY()>y- playerHeight){
+                        clearInterval(id);
+                        id= setInterval(() => {
+                            state.id=id;
+                            socket.emit('move', 'right');
+                            if(players[browserID].getX()>x- playerWidth){
+                                clearInterval(id);
+                                socket.emit('stop','');    
+                                state.moving=false; 
+                            }
+                        }, 50);  
+                    }
+                }, 50);
+            }
+            
+        }
+});
+
 
